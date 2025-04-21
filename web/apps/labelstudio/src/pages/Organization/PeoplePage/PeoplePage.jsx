511356f17295d59826d846ca93019c98a43d7e85
Tom <u8205097@gmail.com>
2025-04-21 08:34:46 +0800

    hide home page images

--- web/apps/labelstudio/src/pages/Organization/PeoplePage/PeoplePage.jsx
+++ web/apps/labelstudio/src/pages/Organization/PeoplePage/PeoplePage.jsx
@@ -149,7 +149,7 @@ export const PeoplePage = () => {
         {selectedUser ? (
           <SelectedUser user={selectedUser} onClose={() => selectUser(null)} />
         ) : (
-          isFF(FF_LSDV_E_297) && <HeidiTips collection="organizationPage" />
+           isFF(FF_LSDV_E_297) //&& <HeidiTips collection="organizationPage" />
         )}
       </Elem>
     </Block>
