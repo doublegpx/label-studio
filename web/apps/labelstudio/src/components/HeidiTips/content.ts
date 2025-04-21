107c71fdadc0d130d09ae22514a9c8f1341cb7d4
Tom <u8205097@gmail.com>
2025-04-21 08:14:27 +0800

    16.3 first check in to org labels

--- web/apps/labelstudio/src/components/HeidiTips/content.ts
+++ web/apps/labelstudio/src/components/HeidiTips/content.ts
@@ -86,90 +86,90 @@ export const defaultTipsCollection: TipsCollection = {
       },
     },
   ],
-  organizationPage: [
-    {
-      title: "It looks like your team is growing!",
-      content:
-        "Assign roles to your team using Label Studio Enterprise and control access to sensitive data at the project and workspace levels.",
-      closable: true,
-      link: {
-        label: "Learn more",
-        url: "https://docs.humansignal.com/guide/manage_users#Roles-in-Label-Studio-Enterprise",
-        params: {
-          experiment: "organization_page_tip",
-          treatment: "team_growing",
-        },
-      },
-    },
-    {
-      title: "Want to simplify and secure logging in?",
-      content: "Enable Single Sign-On for your team using SAML, SCIM2 or LDAP with Label Studio Enterprise.",
-      closable: true,
-      link: {
-        label: "Learn more",
-        url: "https://docs.humansignal.com/guide/auth_setup",
-        params: {
-          experiment: "organization_page_tip",
-          treatment: "enable_sso",
-        },
-      },
-    },
-    {
-      title: "Did you know?",
-      content: "Try Label Studio Starter Cloud, optimized for small teams and projects.",
-      closable: true,
-      link: {
-        label: "Learn more",
-        url: "https://humansignal.com/pricing/",
-        params: {
-          experiment: "organization_page_tip",
-          treatment: "starter_cloud_live",
-        },
-      },
-    },
-    {
-      title: "Want to automate task distribution?",
-      content:
-        "Create rules, automate how tasks are distributed to annotators, and only show tasks assigned to each annotator in their view.and control task visibility for each annotator.",
-      closable: true,
-      link: {
-        label: "Learn more",
-        url: "https://docs.humansignal.com/guide/setup_project#Set-up-annotation-settings-for-your-project",
-        params: {
-          experiment: "organization_page_tip",
-          treatment: "automate_distribution",
-        },
-      },
-    },
-    {
-      title: "Share knowledge with the community",
-      content:
-        "Have questions or a tip to share with other Label Studio users? Join the community slack channel for the latest updates. ",
-      closable: true,
-      link: {
-        label: "Join the community",
-        url: "https://label-studio.slack.com",
-        params: {
-          experiment: "organization_page_tip",
-          treatment: "share_knowledge",
-        },
-      },
-    },
-    {
-      title: "Did you know?",
-      content:
-        "Label Studio supports multiple points of integration with cloud storage, machine learning models, and popular tools to automate your machine learning pipeline.",
-      closable: true,
-      link: {
-        label: "Check out the integrations directory",
-        url: "https://labelstud.io/integrations/",
-        params: {
-          experiment: "organization_page_tip",
-          treatment: "integration_points",
-        },
-      },
-    },
-  ],
+  // organizationPage: [
+  //   {
+  //     title: "It looks like your team is growing!",
+  //     content:
+  //       "Assign roles to your team using Label Studio Enterprise and control access to sensitive data at the project and workspace levels.",
+  //     closable: true,
+  //     link: {
+  //       label: "Learn more",
+  //       url: "https://docs.humansignal.com/guide/manage_users#Roles-in-Label-Studio-Enterprise",
+  //       params: {
+  //         experiment: "organization_page_tip",
+  //         treatment: "team_growing",
+  //       },
+  //     },
+  //   },
+  //   {
+  //     title: "Want to simplify and secure logging in?",
+  //     content: "Enable Single Sign-On for your team using SAML, SCIM2 or LDAP with Label Studio Enterprise.",
+  //     closable: true,
+  //     link: {
+  //       label: "Learn more",
+  //       url: "https://docs.humansignal.com/guide/auth_setup",
+  //       params: {
+  //         experiment: "organization_page_tip",
+  //         treatment: "enable_sso",
+  //       },
+  //     },
+  //   },
+  //   {
+  //     title: "Did you know?",
+  //     content: "Try Label Studio Starter Cloud, optimized for small teams and projects.",
+  //     closable: true,
+  //     link: {
+  //       label: "Learn more",
+  //       url: "https://humansignal.com/pricing/",
+  //       params: {
+  //         experiment: "organization_page_tip",
+  //         treatment: "starter_cloud_live",
+  //       },
+  //     },
+  //   },
+  //   {
+  //     title: "Want to automate task distribution?",
+  //     content:
+  //       "Create rules, automate how tasks are distributed to annotators, and only show tasks assigned to each annotator in their view.and control task visibility for each annotator.",
+  //     closable: true,
+  //     link: {
+  //       label: "Learn more",
+  //       url: "https://docs.humansignal.com/guide/setup_project#Set-up-annotation-settings-for-your-project",
+  //       params: {
+  //         experiment: "organization_page_tip",
+  //         treatment: "automate_distribution",
+  //       },
+  //     },
+  //   },
+  //   {
+  //     title: "Share knowledge with the community",
+  //     content:
+  //       "Have questions or a tip to share with other Label Studio users? Join the community slack channel for the latest updates. ",
+  //     closable: true,
+  //     link: {
+  //       label: "Join the community",
+  //       url: "https://label-studio.slack.com",
+  //       params: {
+  //         experiment: "organization_page_tip",
+  //         treatment: "share_knowledge",
+  //       },
+  //     },
+  //   },
+  //   {
+  //     title: "Did you know?",
+  //     content:
+  //       "Label Studio supports multiple points of integration with cloud storage, machine learning models, and popular tools to automate your machine learning pipeline.",
+  //     closable: true,
+  //     link: {
+  //       label: "Check out the integrations directory",
+  //       url: "https://labelstud.io/integrations/",
+  //       params: {
+  //         experiment: "organization_page_tip",
+  //         treatment: "integration_points",
+  //       },
+  //     },
+  //   },
+  // ],
   projectSettings: [
     {
       title: "Save time with Auto-Labeling",
